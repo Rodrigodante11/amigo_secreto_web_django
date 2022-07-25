4af42d3f2c6858f9d0966427c2c60ffd5d4a4c5b
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-24 23:17:34 -0300

    implementando login e lohout

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import auth
+
 # Create your views here.
 
 
@@ -29,15 +31,37 @@ def cadastro(request):
 
 
 def login(request):
-    if request.method == 'POST':
-        email = request.POST['email']
+    if request.method == 'POST':  # Olhe a pagina login.html  27:   <form action="{%  url 'login' %}" method="POST">
+        email = request.POST['email']  # ['email'] == campo name=`email` da tag <input>
         senha = request.POST['senha']
+        if campo_vazio(email) or campo_vazio(senha):
+            # messages.error(request, 'O campo email e senha nao pode ser vazio')
+            return redirect('login')
+
+        if User.objects.filter(email=email).exists():  # Verificando se o email existe na base de dados
+            nome = User.objects.filter(email=email).values_list('username', flat=True).get()  # pegando o username
+
+            user = auth.authenticate(request, username=nome, password=senha)  # autenticando o usuario
+
+            if user is not None:
+                auth.login(request, user)
+                print('login Sucesso')
+                return redirect('dashboard')
+
     return render(request, 'usuarios/login.html')
 
 
 def logout(request):
-    return render(request, 'usuarios/login.html')
+    auth.logout(request)
+    return redirect('index')
 
 
 def dashboard(request):
-    return render(request, 'usuarios/dashboard.html')
+    if request.user.is_authenticated:
+        return render(request, 'usuarios/dashboard.html')
+    else:
+        return redirect('index')
+
+
+def campo_vazio(campo):
+    return not campo.strip()

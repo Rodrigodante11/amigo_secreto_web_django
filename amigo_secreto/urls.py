642f3df98ccdd051e9434bcc7f5c9bd76b84b73f
Rodrigodante11 <rodrigoaugusto839@gmail.com>
2022-07-24 22:50:16 -0300

    implementando cadastro de usuarios

--- amigo_secreto/urls.py
+++ amigo_secreto/urls.py
@@ -8,7 +8,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('usuarios', include('usuarios.urls')),
+    path('usuarios/', include('usuarios.urls')),
     path('', include('amigos.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # avisando q as rotas podem usar imagens e statics
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,10 +1,43 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 # Create your views here.
+
+
 def cadastro(request):
+    if request.method == 'POST':
+        nome = request.POST['nome']
+        email = request.POST['email']
+        senha = request.POST['senha']
+        senhaConfirmacao = request.POST['senhaConfirmacao']
+
+        if not nome.strip():
+            return redirect('cadastro')
+
+        if not email.strip():
+            return redirect('cadastro')
+
+        if senha != senhaConfirmacao:
+            return redirect('cadastro')
+
+        if User.objects.filter(email=email).exists():  # Verificando se o usuarios ja existe
+            return redirect('cadastro')
+        user = User.objects.create_user(username=nome, email=email, password=senha)
+        user.save()
+        return redirect('login')
+
     return render(request, 'usuarios/cadastro.html')
 
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        senha = request.POST['senha']
     return render(request, 'usuarios/login.html')
+
+
+def logout(request):
+    return render(request, 'usuarios/login.html')
+
+
+def dashboard(request):
+    return render(request, 'usuarios/dashboard.html')
